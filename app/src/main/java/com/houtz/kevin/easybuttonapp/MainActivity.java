e009e1b738b29cb0b2fb56af147161f22c27540d
khoutz182 <khoutz182@gmail.com>
2015-04-01 18:10:30 -0500

    initial

---
+++ app/src/main/java/com/houtz/kevin/easybuttonapp/MainActivity.java
@@ -0,0 +1,75 @@
+package com.houtz.kevin.easybuttonapp;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentStatePagerAdapter;
+import android.support.v4.view.PagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+
+public class MainActivity extends FragmentActivity {
+
+    private static final int NUM_PAGES = 3;
+    private ViewPager viewPager;
+    private PagerAdapter pagerAdapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        viewPager = (ViewPager) findViewById(R.id.pager);
+        pagerAdapter = new ScreenSlidePagerAdapter(getSupportFragmentManager());
+        viewPager.setAdapter(pagerAdapter);
+        viewPager.setPageTransformer(true, new ZoomOutPageTransformer());
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+
+    private class ScreenSlidePagerAdapter extends FragmentStatePagerAdapter {
+        public ScreenSlidePagerAdapter(FragmentManager fm) {
+            super(fm);
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            ScreenSlideFragment frag = new ScreenSlideFragment();
+            Bundle bundle = new Bundle();
+            bundle.putInt("PAGE_NUM", new Integer(position));
+            frag.setArguments(bundle);
+            return frag;
+        }
+
+        @Override
+        public int getCount() {
+            return NUM_PAGES;
+        }
+    }
+
+}
---
+++ app/src/main/java/com/houtz/kevin/easybuttonapp/ScreenSlideFragment.java
@@ -0,0 +1,49 @@
+package com.houtz.kevin.easybuttonapp;
+
+
+import android.media.Image;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageButton;
+
+/**
+ * Created by k.houtz on 4/1/2015.
+ */
+public class ScreenSlideFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        int pageNum = 0;
+        try {
+            pageNum = ((Integer)getArguments().get("PAGE_NUM")).intValue();
+        } catch(Exception e) {}
+
+
+
+        ViewGroup rootView = (ViewGroup) inflater.inflate(
+            R.layout.fragment_screen_slide_page, container, false
+        );
+
+        if(rootView.getChildCount() > 0 && rootView.getChildAt(0) instanceof ImageButton) {
+            ImageButton button = (ImageButton)rootView.getChildAt(0);
+            button.setOnClickListener(new MyClickListener(pageNum));
+            switch(pageNum) {
+                case 0: button.setImageResource(R.drawable.easy_full);
+                    break;
+                case 1: button.setImageResource(R.drawable.hard_full);
+                    break;
+                case 2: button.setImageResource(R.drawable.she_said_full);
+                    break;
+                default: button.setImageResource(R.drawable.easy_full);
+                    break;
+            }
+
+
+        }
+
+        return rootView;
+    }
+}
